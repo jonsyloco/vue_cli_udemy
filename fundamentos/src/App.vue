3ec67d81c2bc9367d68d1c475cf83fb20f3551ea
jhonatan wagner ocampo <jonsyloco@gmail.com>
2021-12-11 18:15:08 -0500

    inicio de una app

---
+++ fundamentos/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <counter/>
+</template>
+
+<script>
+import Counter from './components/Counter.vue';
+export default {
+  components: { Counter },
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ fundamentos/src/components/Counter.vue
@@ -0,0 +1,54 @@
+<template>
+  <h2>Counter</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ getSquareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ getSquareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ getSquareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ getSquareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ getSquareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ getSquareCounter }}</p>
+ 
+    <div>
+        <button>+1</button>
+        <button>-1</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      counter: 5,
+    };
+  },
+  methods: {
+    getSquareValue() {
+        console.log("getSquareValue");
+        return this.counter * this.counter;
+    },
+  },
+  computed: {
+      getSquareCounter() {
+          console.log("computed");
+          return this.getSquareValue();
+      }
+  },
+};
+</script>
+
+<style>
+    button{
+        background-color: aquamarine;
+        border-radius: 5px;
+        border: 1px solid white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+    }
+
+    button:hover{
+        background-color: yellow;
+        transition: 0.3s;
+    }
+</style>
